03ebb52c22c771429b89ebf4df63f8b3a004da11
bkhmondocteur <bkhmondocteur@gmail.com>
2026-05-14 13:31:20 +0100

    Update service-worker.js

--- DocPocket-Pro/service-worker.js
+++ DocPocket-Pro/service-worker.js
@@ -1,24 +1,24 @@
-const CACHE_NAME = 'docpocket-cache-v1';
-const urlsToCache = [
-'/',
-'/index.html',
-'/style.css',
-'/app.js',
-'/manifest.json'
+const CACHE_NAME = 'docpocket-pro-v1';
+const ASSETS = [
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json',
+  'logo.png'
 ];
-self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => {
-return cache.addAll(urlsToCache);
-})
-);
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
-self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => {
-return response || fetch(event.request);
-})
-);
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
